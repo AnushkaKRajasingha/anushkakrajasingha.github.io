73d6413aa2b67c8b084b5326e347071cbde7175d
Anushka Rajasingha <AnushkaKRajasingha@users.noreply.github.com>
2022-09-02 14:07:46 +0530

    php test

---
+++ blog.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Blog - Anushka K Rajasingha</title>
+    <meta name="description" content="Wordpress Developer | DIVI / Elementor Developer.">
+</head>
+<body>
+<h1>Blog</h1>
+<p>Blog post goes here</p>
+</body>
+</html>
